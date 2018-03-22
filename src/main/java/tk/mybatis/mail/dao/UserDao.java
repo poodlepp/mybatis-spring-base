f9168456065dabbe88cf55a0f328ea2ca6c39107
poodlepp <pengyili07@gmail.com>
2018-03-22 18:51:00 +0800

    add:mail-bean,dao,controller

---
+++ src/main/java/tk/mybatis/mail/dao/UserDao.java
@@ -0,0 +1,7 @@
+package tk.mybatis.mail.dao;
+
+import tk.mybatis.mail.bean.User;
+
+public interface UserDao {
+	void insertUser(User user);
+}
---
+++ src/main/java/tk/mybatis/mail/service/UserService.java
@@ -0,0 +1,7 @@
+package tk.mybatis.mail.service;
+
+import tk.mybatis.mail.bean.User;
+
+public interface UserService {
+	void regist(User user);
+}
